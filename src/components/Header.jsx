26073a8c54d5847aaab776fd42bff963c0694828
tomohiro19840727 <tomohirofarm@gmail.com>
2025-11-03 19:40:01 +0900

    変更した

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
           <svg width="95" height="94" viewBox="0 0 95 94" className="h-auto w-6 text-indigo-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path d="M96 0V47L48 94H0V47L48 0H96Z" />
           </svg>
-          FARM ひまわり
+          FARM ひま
         </Link>
 
         <nav className="hidden gap-12 lg:flex">
